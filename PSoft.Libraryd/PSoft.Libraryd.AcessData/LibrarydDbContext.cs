a01608120bb323129f8ea11b9eed127210484561
n-caro <niicolas.caro@gmail.com>
2020-09-22 04:04:19 -0300

    Config EFCore

---
+++ PSoft.Libraryd/PSoft.Libraryd.AcessData/LibrarydDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSoft.Libraryd.AcessData
+{
+    class LibrarydDbContext : DbContext
+    {
+        // add DBset here
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=LibrarydDBdev;Trusted_Connection=True;");
+        }
+    }
+}
